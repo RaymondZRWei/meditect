8c178d83f01634c7ca0e6c4c681332c7dbd1103d
inconspicuousaltacc <inconspicuousaltacc@gmail.com>
2024-07-12 12:18:58 -0400

    medicines all implemented

--- src/lib/data/medicine.ts
+++ src/lib/data/medicine.ts
@@ -1,193 +1,105 @@
 import type { GameData, Medicine } from "$lib/types";
 
 export const medicines: Medicine[] = [
-    {
-        name: "Antibiotics",
-        description: "",
-        duration: 5,
-        updateGame: (game) => {
-            game.disease.arbitraryValues.opioid += 50;
-            game.disease.arbitraryValues.opioid += 50;
+  {
+    name: "Naloxone",
+    description: "",
+    duration: 5,
+    updateGame: (game) => {
+      if (!game.disease.arbitraryValues.opioid) return game;
 
-            return game;
-        },
+      game.disease.arbitraryValues.opioid -= 50;
+      return game;
     },
-    {
-        name: "Naloxone",
-        description: "",
-        duration: 5,
-        updateGame: (game) => {
-            if (!game.disease.arbitraryValues.opioid) return game;
+  },
+  {
+    name: "Rivaroxaban",
+    description: "",
+    duration: 5,
+    updateGame: (game) => {
+      if (!game.disease.arbitraryValues.pulmonary) return game;
 
-            game.disease.arbitraryValues.opioid -= 50;
-            return game;
-        },
+      game.disease.arbitraryValues.pulmonary -= 50;
+      return game;
     },
-    {
-        name: "Morphine",
-        description: "",
-        duration: 5,
-        updateGame: (game) => {
-            // // Side effect if opioid is not present
-            // if (!game.disease.arbitraryValues.opioid) {
-            //     game.heartRate.value /= 1.25;
-            //     return game;
-            // }
+  },
+  {
+    name: "Inhaler (Puffer)",
+    description: "",
+    duration: 5,
+    updateGame: (game) => {
+      if (!game.disease.arbitraryValues.asthma) return game;
 
-            game.disease.arbitraryValues.opioid += 50;
-
-            return game;
-        },
+      game.disease.arbitraryValues.asthma -= 50;
+      return game;
     },
-    {
-        name: "Rivaroxaban",
-        description: "",
-        duration: 5,
-        updateGame: (game) => {
-            if (!game.disease.arbitraryValues.opioid) return game;
+  },
+  {
+    name: "Thrombolytics",
+    description: "",
+    duration: 5,
+    updateGame: (game) => {
+      if (!game.disease.arbitraryValues.heart) return game;
+      game.disease.arbitraryValues.heart -= 50;
 
-            game.disease.arbitraryValues.opioid -= 50;
-            return game;
-        },
+      return game;
     },
-    {
-        name: "Heparin",
-        description: "",
-        duration: 5,
-        updateGame: (game) => {
-            game.disease.arbitraryValues.opioid += 50;
+  },
+  {
+    name: "Intravenous Crystalloid IV",
+    description: "",
+    duration: 5,
+    updateGame: (game) => {
+      if (!game.disease.arbitraryValues.acute && !game.disease.arbitraryValues.sepsis) return game;
+      if (game.disease.arbitraryValues.acute) {
+        game.disease.arbitraryValues.acute -= 50;
+      }
+      if (game.disease.arbitraryValues.acute) {
+        game.disease.arbitraryValues.sepsis -= 50;
+      }
+      return game;
+    },
+  },
+  {
+    name: "Loperamide",
+    description: "",
+    duration: 5,
+    updateGame: (game) => {
+      if (!game.disease.arbitraryValues.food) return game;
+      game.disease.arbitraryValues.food -= 50;
+      return game;
+    },
+  },
+  {
+    name: "Nafcillin",
+    description: "",
+    duration: 5,
+    updateGame: (game) => {
+      if (!game.disease.arbitraryValues.staph) return game;
+      game.disease.arbitraryValues.staph -= 50;
 
-            return game;
-        },
+      return game;
     },
-    {
-        name: "Warfarin",
-        description: "",
-        duration: 5,
-        updateGame: (game) => {
-            game.disease.arbitraryValues.opioid += 50;
+  },
+  {
+    name: "Ibuprofen",
+    description: "",
+    duration: 5,
+    updateGame: (game) => {
+      if (!game.disease.arbitraryValues.laceration) return game;
+      game.disease.arbitraryValues.laceration -= 50;
 
-            return game;
-        },
+      return game;
     },
-    {
-        name: "Thrombolytics",
-        description: "",
-        duration: 5,
-        updateGame: (game) => {
-            game.disease.arbitraryValues.opioid += 50;
-
-            return game;
-        },
-    },
-    {
-        name: "Fibrinolytics",
-        description: "",
-        duration: 5,
-        updateGame: (game) => {
-            game.disease.arbitraryValues.opioid += 50;
-
-            return game;
-        },
-    },
-    {
-        name: "Nitroglycerin",
-        description: "",
-        duration: 5,
-        updateGame: (game) => {
-            game.disease.arbitraryValues.opioid += 50;
-
-            return game;
-        },
-    },
-    {
-        name: "Intravenous Crystalloid IV",
-        description: "",
-        duration: 5,
-        updateGame: (game) => {
-            game.disease.arbitraryValues.opioid += 50;
-
-            return game;
-        },
-    },
-    {
-        name: "Loperamide",
-        description: "",
-        duration: 5,
-        updateGame: (game) => {
-            game.disease.arbitraryValues.opioid += 50;
-
-            return game;
-        },
-    },
-    {
-        name: "Ondansetron",
-        description: "",
-        duration: 5,
-        updateGame: (game) => {
-            game.disease.arbitraryValues.opioid += 50;
-
-            return game;
-        },
-    },
-    {
-        name: "Nafcillin",
-        description: "",
-        duration: 5,
-        updateGame: (game) => {
-            game.disease.arbitraryValues.opioid += 50;
-
-            return game;
-        },
-    },
-    {
-        name: "Vancomycin",
-        description: "",
-        duration: 5,
-        updateGame: (game) => {
-            game.disease.arbitraryValues.opioid += 50;
-
-            return game;
-        },
-    },
-    {
-        name: "Ibuprofen",
-        description: "",
-        duration: 5,
-        updateGame: (game) => {
-            game.disease.arbitraryValues.opioid += 50;
-
-            return game;
-        },
-    },
-    {
-        name: "Acetaminophen",
-        description: "",
-        duration: 5,
-        updateGame: (game) => {
-            game.disease.arbitraryValues.opioid += 50;
-
-            return game;
-        },
-    },
-    {
-        name: "Tetanus Prophylaxis",
-        description: "",
-        duration: 5,
-        updateGame: (game) => {
-            game.disease.arbitraryValues.opioid += 50;
-
-            return game;
-        },
-    },
+  },
 ];
 
 export const updateGameWithMedicine = (
-    game: GameData,
-    medicine: Medicine,
+  game: GameData,
+  medicine: Medicine,
 ): GameData => {
-    game = medicine.updateGame(game);
-    game.elapsedTime += medicine.duration;
+  game = medicine.updateGame(game);
+  game.elapsedTime += medicine.duration;
 
-    return game;
+  return game;
 };

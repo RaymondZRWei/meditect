07e8e451a5dfc1a102dde74c53ed95fed81f826f
principle105 <principle105@gmail.com>
2024-07-12 17:32:22 -0400

    improved medicine page styling and selection

--- src/lib/components/Stat.svelte
+++ src/lib/components/Stat.svelte
@@ -1,4 +1,20 @@
 <script lang="ts">
+    import { createTooltip, melt } from "@melt-ui/svelte";
+    import { fade } from "svelte/transition";
+
+    const {
+        elements: { trigger, content, arrow },
+        states: { open },
+    } = createTooltip({
+        positioning: {
+            placement: "right-end",
+        },
+        openDelay: 0,
+        closeDelay: 0,
+        closeOnPointerDown: false,
+        forceVisible: true,
+    });
+
     export let title: string;
     export let value: number;
     export let unit: string;
@@ -6,9 +22,9 @@
 </script>
 
 <div
-    class="bg-slate-100 w-full h-full rounded-xl p-8 outline-none flex items-center justify-between"
+    class="bg-slate-100 w-full h-full rounded-xl p-6 outline-none flex items-center justify-between"
 >
-    <div class="flex flex-col gap-3.5">
+    <div class="flex flex-col gap-3.5 cursor-pointer" use:melt={$trigger}>
         <div class="text-black font-medium mb-0.5">{title}</div>
 
         <div class="flex items-end gap-1.5 text-slate-600">
@@ -18,3 +34,14 @@
     </div>
     <slot />
 </div>
+
+{#if $open}
+    <div
+        use:melt={$content}
+        transition:fade={{ duration: 100 }}
+        class="z-10 rounded-lg bg-slate-600 shadow"
+    >
+        <div use:melt={$arrow} />
+        <p class="px-4 py-1 text-white">{tooltip}</p>
+    </div>
+{/if}

9217386c09a65e1fff2647f1f39d079de3f1910a
inconspicuousaltacc <inconspicuousaltacc@gmail.com>
2024-07-12 22:22:12 -0400

    change circles

--- src/lib/data/tests.ts
+++ src/lib/data/tests.ts
@@ -1,36 +1,36 @@
 import type { Test } from "../types";
 
 export const tests: Test[] = [
-    {
-        name: "Respiratory Rate",
-        unit: "Breaths per Min",
-        actionMessage: "Use Spirometer",
-        queriedSymptom: "respiratoryRate",
-        maxValue: 30,
-        duration: 2,
-    },
-    {
-        name: "Oxygen Saturation",
-        unit: "% Sp02",
-        actionMessage: "Use Oximeter",
-        queriedSymptom: "oxygenSaturation",
-        maxValue: 100,
-        duration: 2,
-    },
-    {
-        name: "Blood Glucose",
-        unit: "mg / dL",
-        actionMessage: "Use Glucometer",
-        queriedSymptom: "bloodGlucose",
-        maxValue: 100,
-        duration: 2,
-    },
-    {
-        name: "Pain Response",
-        unit: "Scale 0-100",
-        actionMessage: "Ask Patient",
-        queriedSymptom: "pain",
-        maxValue: 100,
-        duration: 1,
-    },
+  {
+      name: "Respiratory Rate",
+      unit: "Breaths per Min",
+      actionMessage: "Use Spirometer",
+      queriedSymptom: "respiratoryRate",
+      maxValue: 40,
+      duration: 2,
+  },
+  {
+      name: "Oxygen Saturation",
+      unit: "% Sp02",
+      actionMessage: "Use Oximeter",
+      queriedSymptom: "oxygenSaturation",
+      maxValue: 100,
+      duration: 2,
+  },
+  {
+      name: "Blood Glucose",
+      unit: "mg / dL",
+      actionMessage: "Use Glucometer",
+      queriedSymptom: "bloodGlucose",
+      maxValue: 150,
+      duration: 2,
+  },
+  {
+      name: "Pain Level",
+      unit: "Scale 0-100",
+      actionMessage: "Ask Patient",
+      queriedSymptom: "pain",
+      maxValue: 100,
+      duration: 1,
+  },
 ];
